6c135eaea995ae76a79f0b134d5d918a29ff93a9
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2024-06-08 08:43:06 +0530

    doing some tests

--- testerer.py
+++ testerer.py
@@ -1,5 +1,6 @@
 from cloudscraper import create_scraper
 import aiohttp, requests
+from aiocfscrape import CloudflareScraper
 
 
 async def main():
@@ -11,18 +12,18 @@ async def main():
             print(response.headers)
 
     print("cloudscraper")
-
     s = create_scraper()
     response = s.get(
         "https://vadapav.mov/f/e6a15357-f5a3-49e9-a887-c5cd0d29fc35/", stream=True
     )
     print(response.headers)
 
-    print("requests")
-    response = requests.get(
-        "https://vadapav.mov/f/e6a15357-f5a3-49e9-a887-c5cd0d29fc35/"
-    )
-    print(response.headers)
+    print("aiocfscrape")
+    async with CloudflareScraper as session:
+        async with session.get(
+            "https://vadapav.mov/f/e6a15357-f5a3-49e9-a887-c5cd0d29fc35/"
+        ) as response:
+            print(response.headers)
 
 
 import asyncio
